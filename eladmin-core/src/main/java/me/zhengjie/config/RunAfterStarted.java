d1b10c670ba368173175e4746d87297e25670866
jiangquanxing <jiangquanxing@sinog2c.com>
2022-11-10 09:58:49 +0800

    框架改造

--- eladmin-core/src/main/java/me/zhengjie/config/RunAfterStarted.java
+++ eladmin-core/src/main/java/me/zhengjie/config/RunAfterStarted.java
@@ -15,15 +15,15 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 
 /**
- * @ClassName: LoadAfterStarted
- * @Description:
+ * @ClassName: RunAfterStarted
+ * @Description: 项目启动后运行
  * @Author: jiangqx
  * @Date: 2022/11/8 10:14
  * @Version 1.0
  */
 @Slf4j
 @Component
-public class LoadAfterStarted implements CommandLineRunner {
+public class RunAfterStarted implements CommandLineRunner {
 
     @Resource
     Environment environment;
@@ -45,10 +45,15 @@ public class LoadAfterStarted implements CommandLineRunner {
         //设置返回媒体数据类型
         headers.add("Accept", MediaType.APPLICATION_JSON.toString());
         log.info(">>>>>>>>>>>>>>> 加载 yauaa-useragent >>>>>>>>>>>>>>>");
-        // 验证码接口
-        restTemplate.execute("http://127.0.0.1:" + environment.getProperty("local.server.port") + "/auth/getVerificationCode", HttpMethod.GET, null, null);
-        // 登录接口
-        restTemplate.postForObject("http://127.0.0.1:" + environment.getProperty("local.server.port") + "/auth/login", new HttpEntity<>(new JSONObject().toString(), headers), String.class);
-        log.info(">>>>>>>>>>>>>>> 项目启动完成 >>>>>>>>>>>>>>>");
+        try {
+            // 验证码接口
+            restTemplate.execute("http://127.0.0.1:" + environment.getProperty("local.server.port") + "/auth/getVerificationCode", HttpMethod.GET, null, null);
+            // 登录接口
+            restTemplate.postForObject("http://127.0.0.1:" + environment.getProperty("local.server.port") + "/auth/login", new HttpEntity<>(new JSONObject().toString(), headers), String.class);
+        } catch (Exception e) {
+
+        } finally {
+            log.info(">>>>>>>>>>>>>>> 项目启动完成 >>>>>>>>>>>>>>>");
+        }
     }
 }
